bf0832e982d4a13e3f8a177940be5a7181ce80c3
Riaz <almahmudr1@gmail.com>
2021-08-14 21:39:37 +0600

    asdfa

--- app/src/main/java/org/tensorflow/lite/backdoor/it/IncomingObjectActivity.java
+++ app/src/main/java/org/tensorflow/lite/backdoor/it/IncomingObjectActivity.java
@@ -1,7 +1,5 @@
 package org.tensorflow.lite.backdoor.it;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -9,9 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class IncomingObjectActivity extends AppCompatActivity {
 
@@ -22,6 +25,7 @@ public class IncomingObjectActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private TextView textView;
     MediaPlayer alarm;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +38,34 @@ public class IncomingObjectActivity extends AppCompatActivity {
         sensorManager = (SensorManager) getApplicationContext().getSystemService(SENSOR_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        if (proximitySensor == null){
+        if (proximitySensor == null) {
             Toast.makeText(this, "Proximity Sensor is not Available", Toast.LENGTH_SHORT).show();
-        }else {
+        } else {
             proximitySensorListener = new SensorEventListener() {
                 @Override
                 public void onSensorChanged(SensorEvent event) {
-                    if (event.values[0]< proximitySensor.getMaximumRange()){
-                        if (alarm.isPlaying()){
+                    if (event.values[0] < proximitySensor.getMaximumRange()) {
+
+                        textToSpeech = new TextToSpeech(getApplicationContext(), status -> {
+                            if (status != TextToSpeech.ERROR) {
+                                // To Choose language of speech
+                                if (textToSpeech.isSpeaking()) {
+                                    textToSpeech.stop();
+                                }
+                                textToSpeech.setLanguage(Locale.US);
+                                textToSpeech.speak("Incoming Object Detect", TextToSpeech.QUEUE_ADD, null);
+                            }
+                        });
+
+                        if (alarm.isPlaying()) {
                             alarm.seekTo(0);
-                        }else{
+                        } else {
                             alarm.start();
                         }
 
                         linearLayout.setBackgroundColor(Color.RED);
                         textView.setText("Incoming Object Detect");
-                    }else {
+                    } else {
                         if (alarm.isPlaying()) {
                             alarm.pause();
                         }
--- app/src/main/java/org/tensorflow/lite/backdoor/it/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/backdoor/it/MainActivity.java
@@ -6,18 +6,18 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
-    private CardView currencyDetectBtn,colorSelectBtn,incomingObjectBtn;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currencyDetectBtn = findViewById(R.id.currencyDetectBtn);
-        colorSelectBtn = findViewById(R.id.colorSelectBtn);
-        incomingObjectBtn = findViewById(R.id.incomingObjectCard);
+        CardView currencyDetectBtn = findViewById(R.id.currencyDetectBtn);
+        CardView colorSelectBtn = findViewById(R.id.colorSelectBtn);
+        CardView incomingObjectBtn = findViewById(R.id.incomingObjectCard);
+        CardView objectDetectBtn = findViewById(R.id.objectDetectBtn);
 
         currencyDetectBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ClassifierActivity.class)));
         colorSelectBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ColorPaletteActivity.class)));
